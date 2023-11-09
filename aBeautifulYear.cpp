dde98fc5c4f6d82b4a0a12fc9fb4adbbdafef5aa
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2023-11-09 16:17:43 +0200

    A Beatiful Year Solution

---
+++ aBeautifulYear.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+#include <iostream>
+#include <string>
+#include "math.h"
+
+bool isDistinct(int year) {
+    string year_s = to_string(year);
+    
+    return (year_s[0] != year_s[1] && year_s[0] != year_s[2] && year_s[0] != year_s[3] && year_s[1] != year_s[2] && year_s[1] != year_s[3] && year_s[2] != year_s[3]);
+}
+
+int main() {
+    int input;
+    int year;
+    cin >> input;
+    year = input;
+    do {
+        year++;
+    } while (!isDistinct(year));
+    
+    cout << year << endl;
+    return 0;
+}
+
